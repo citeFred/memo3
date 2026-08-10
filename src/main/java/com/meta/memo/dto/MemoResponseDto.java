8bbeeea9583be02296b23329ec863b74aab9380c
citeFred <citefred@gmail.com>
2026-08-10 15:44:31 +0900

    feat: 메모생성(Create) 기초 기능 구현 완료

--- src/main/java/com/meta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/meta/memo/dto/MemoResponseDto.java
@@ -1,5 +1,6 @@
 package com.meta.memo.dto;
 
+import com.meta.memo.domain.Memo;
 import lombok.Getter;
 
 @Getter
@@ -7,4 +8,10 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+
+    public MemoResponseDto(Memo memo) {
+        this.id = memo.getId();
+        this.username = memo.getUsername();
+        this.contents = memo.getContents();
+    }
 }
