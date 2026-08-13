0c3882c1fc140f2dae3d65f0d6277056ed192baf
citeFred <citefred@gmail.com>
2026-08-13 14:29:19 +0900

    refactor: 3Layer - 메서드마다 객체 중복 생성 제거

--- src/main/java/com/meta/memo/service/MemoService.java
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -9,17 +9,16 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 public class MemoService {
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoRepository memoRepository;
 
     public MemoService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoRepository = new MemoRepository(jdbcTemplate);
     }
 
     public MemoResponseDto createMemo(MemoRequestDto memoRequestDto) {
         // RequestDto -> Entity 변환
         Memo memo = new Memo(memoRequestDto);
 
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         Memo savedMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto 변환
@@ -29,14 +28,11 @@ public class MemoService {
 
 
     public List<MemoResponseDto> getMemos() {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
         List<MemoResponseDto> responseDtoList = memoRepository.findAll();
         return responseDtoList;
     }
 
     public Long updateMemo(Long id, MemoRequestDto memoRequestDto) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-
         // 해당 id의 메모가 데이터베이스에 존재하는지 확인
         Memo foundMemo = memoRepository.findById(id);
 
@@ -50,8 +46,6 @@ public class MemoService {
     }
 
     public Long deleteMemo(Long id) {
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-
         // 해당 id의 메모가 데이터베이스에 존재하는지 확인
         Memo foundMemo = memoRepository.findById(id);
 
